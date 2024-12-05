ef0e109e77848cea9e860439e53fef61b51e9301
Madeline <grammaticomadeline@hotmail.fr>
2024-12-05 17:52:48 +0100

    TSE-02 : THEME:  fix change index.js to theme.ts and keep both

--- src/components/pages/order/Navbar/ToastAdmin.jsx
+++ src/components/pages/order/Navbar/ToastAdmin.jsx
@@ -1,7 +1,7 @@
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import styled from "styled-components"
-import { theme } from "../../../../theme/theme"
+import { theme } from "../../../../theme"
 
 export default function ToastAdmin() {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client"
 import App from "./App"
 import "./index.css"
 import { ThemeProvider } from "styled-components"
-import { theme } from "./theme/theme.js"
+import { theme } from "./theme.js"
 import { BrowserRouter } from "react-router-dom"
 
 const rootElement = document.getElementById("root")
