72f02f5ffe771ff1d5d8900bd3e9a0e06b648f6f
Madeline <grammaticomadeline@hotmail.fr>
2024-12-06 23:07:22 +0100

    TSE-03 : UTILS : fix array.ts and window.ts types

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,41 +1,37 @@
-export const deepClone = (array: object) => {
+export const deepClone = <T>(array: T[]): T[]=> {
   return JSON.parse(JSON.stringify(array))
 }
 
+type IdType = string | number
 // utilise un type generique qui utilise l'inférence
-export const findObjectById = <T extends {id: string}>(
-  id: string,
+export const findObjectById = <T extends {id: IdType}>(
+  id: IdType,
   array: T[]
 ): T | undefined => {
   return array.find((itemInArray) => itemInArray.id === id)
 }
 
 //sans utiliser de generique et sans inférence avec unknown et Record
-export const findIndexById = (
-  idWithUnknowwIndex: string, 
-  array: ({id: string} & Record<string, unknown>)[]
-) : number | undefined=> {
+// export const findIndexById = (
+//   idWithUnknowwIndex: IdType, 
+//   array: ({id: IdType} & Record<string, unknown>)[]
+// ) : number => {
+//   return array.findIndex((itemInArray) => itemInArray.id === idWithUnknowwIndex)
+// }
+export const findIndexById = <T extends {id: IdType}>(
+  idWithUnknowwIndex: IdType, 
+  array: T[]
+) : number => {
   return array.findIndex((itemInArray) => itemInArray.id === idWithUnknowwIndex)
 }
 
-export const removeObjectById = <T extends {id: string}>(
-  idOfItemToRemove: string, 
+export const removeObjectById = <T extends {id: IdType}>(
+  idOfItemToRemove: IdType, 
   array: T[]
-) : T[] |undefined => {
+) : T[] => {
   return array.filter((item) => item.id !== idOfItemToRemove)
 }
 
-export const isEmpty = <T extends {id: string}>(array: T[]) : boolean => {
+export const isEmpty = (array: []) : boolean => {
   return array.length === 0
 }
-
-// const fruits = [{ nom: "Abricot" }, { nom: "Banane" }]
-// const fruitsShallowCopy = [...fruits]
-// const fruitsDeepCopy = deepClone(fruits)
-
-// //fruitsShallowCopy[1].nom = "Cerise"
-// fruitsDeepCopy[1].nom = "Cerise"
-
-// console.log("fruits: ", fruits)
-// //console.log("fruitsShallowCopy: ", fruitsShallowCopy)
-// console.log("fruitsDeepCopy: ", fruitsDeepCopy)
