2f4404aa00404866ddc5b16d4a184cc04f477712
Madeline <grammaticomadeline@hotmail.fr>
2024-12-11 18:17:42 +0100

    TSE-06 : API :  add type to Product

--- src/api/product.ts
+++ src/api/product.ts
@@ -1,7 +1,8 @@
 import { doc, getDoc, setDoc } from "firebase/firestore"
 import { db } from "./firebase-config"
+import { MenuType } from "../types/commons"
 
-export const syncBothMenus = (userId, menuUpdated) => {
+export const syncBothMenus = (userId: string, menuUpdated: MenuType) => {
   const cachette = doc(db, "users", userId)
 
   const nourriture = {
@@ -11,13 +12,13 @@ export const syncBothMenus = (userId, menuUpdated) => {
   setDoc(cachette, nourriture)
 }
 
-export const getMenu = async (idUser) => {
+export const getMenu = async (idUser: string) => {
   //const docRef = doc(CHEMIN)
   const docRef = doc(db, "users", idUser)
 
   const docSnapshot = await getDoc(docRef)
   if (docSnapshot.exists()) {
-    const { menu } = docSnapshot.data()
-    return menu
+    const { menu} = docSnapshot.data()
+    return menu as MenuType
   }
 }
