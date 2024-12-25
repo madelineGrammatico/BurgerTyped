ccae210e39da63a60316eae5368554e9951ca03b
Madeline <grammaticomadeline@hotmail.fr>
2024-12-25 23:24:57 +0100

    TSE-08 : ROUTER :  add Router.tsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,20 @@
 import { createRoot } from "react-dom/client"
-import App from "./App"
 import "./index.css"
 import { ThemeProvider } from "styled-components"
 import { theme } from "./theme"
-import { BrowserRouter } from "react-router-dom"
+import { RouterProvider } from "react-router-dom"
+import { router } from "./routers/router"
 import { OrderContextProvider } from "./context/OrderContext"
 
 const rootElement = document.getElementById("root")
 
 if (rootElement) {
   createRoot(rootElement).render(
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <OrderContextProvider>
-          <App />
-        </OrderContextProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+    <OrderContextProvider>
+      <RouterProvider router={router} />
+    </OrderContextProvider>
+  </ThemeProvider>
   )
 } else {
   console.error("Root element with'root' not found.")
---
+++ src/routers/router.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom"
+//@ts-ignore
+import LoginPage from "../components/pages/login/LoginPage"
+//@ts-ignore
+import OrderPage from "../components/pages/order/OrderPage"
+import ErrorPage from "../components/pages/error/ErrorPage"
+
+
+const routes = [
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
+  {
+    path: "/order/:username",
+    element: <OrderPage />,
+  },
+  {
+    path: "*", // "catch-all" route
+    element: <ErrorPage />,
+  },
+]
+
+export const router = createBrowserRouter(routes)
