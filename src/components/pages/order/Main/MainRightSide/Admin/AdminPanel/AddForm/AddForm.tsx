9f4ae8d47244ef59f43e819f5dcd8048b08366e7
Madeline <grammaticomadeline@hotmail.fr>
2025-01-06 18:41:07 +0100

    TSE-14 :  ADMIN : fix AddForm.tsx

--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/AddForm/AddForm.tsx
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/AddForm/AddForm.tsx
@@ -1,8 +1,8 @@
 import { useParams } from "react-router-dom"
-import { useOrderContext } from "../../../../../../../../context/OrderContext"
-import { EMPTY_PRODUCT } from "../../../../../../../../enums/product"
-import { useSuccessMessage } from "../../../../../../../../hooks/useSuccessMessage"
-import { replaceFrenchCommaWithDot } from "../../../../../../../../utils/maths"
+import { useOrderContext } from "@/context/OrderContext"
+import { EMPTY_PRODUCT } from "@/enums/product"
+import { useSuccessMessage } from "@/hooks/useSuccessMessage" 
+import { replaceFrenchCommaWithDot } from "@/utils/maths" 
 import Form from "../Form/Form"
 import SubmitButton from "./SubmitButton"
 
